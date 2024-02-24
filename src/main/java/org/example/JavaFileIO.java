62f694d8ac897ccc54ff96aaeb34184001494668
Abdul Qader <gm.abdul.qader@gmail.com>
2024-02-25 03:24:00 +0600

    initial commit

--- src/main/java/org/example/JavaFileIO.java
+++ src/main/java/org/example/JavaFileIO.java
@@ -4,14 +4,14 @@ import java.io.*;
 
 public class JavaFileIO {
     public static void main(String[] args) {
-//        try {
-//            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
-//            writer.write("Writing into a file");
-//            writer.write("\nWriting into a file");
-//            writer.close();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write("Writing into a file");
+            writer.write("\nWriting into a file");
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
